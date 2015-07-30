ac5beddbc698904d2753b79041b4cb50cedc9e5e
pjwoo59 <pjwoo59@naver.com>
2015-07-30 21:54:05 +0900

    Create MeanMode.rb

---
+++ MeanMode.rb
@@ -0,0 +1,11 @@
+def MeanMode(arr)
+
+  mode = arr.max_by{|n| arr.count(n)}
+  # code goes here
+  return mode == arr.sort[(arr.length.to_f / 2).round] ? 1 : 0
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+MeanMode(STDIN.gets)           
